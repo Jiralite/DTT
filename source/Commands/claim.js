1da342ca70da2f8a62bfde205c5a51c3042acf52
Jiralite <Jiralite@Live.co.uk>
2021-06-10 22:20:31 +0100

    fix(claim): Remove the role upon claiming a request

--- source/Commands/claim.js
+++ source/Commands/claim.js
@@ -26,7 +26,7 @@ class claim {
 
     FreeBugMail.fetchMessage().then(async message => {
       await message.react("<a:typing:852637406334156800>");
-      if (!interaction.member.roles.cache.has("852589448070692947")) await interaction.member.roles.remove("852589448070692947");
+      if (interaction.member.roles.cache.has("852589448070692947")) await interaction.member.roles.remove("852589448070692947");
 
       interaction.reply({
         content: `You have successfully claimed the free BugMail request of <@${FreeBugMail.userId}>!`,
