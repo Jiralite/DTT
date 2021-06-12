c873223e8c091899fd50240c5e011e323178d694
Jiralite <Jiralite@Live.co.uk>
2021-06-12 02:14:35 +0100

    feat(freeBugMail): Add in information on what to do next

--- source/Client/FreeBugMail.js
+++ source/Client/FreeBugMail.js
@@ -186,7 +186,7 @@ class FreeBugMail {
 
       message.react("<a:typing:852637406334156800>");
       if (interaction.member.roles.cache.has(this.#DTT.role("Free BugMail").id)) interaction.member.roles.remove(this.#DTT.role("Free BugMail"));
-      this.#DTT.kanal("bugmail-discussion").send(`${interaction.user} has just claimed the free BugMail request of <@${this.userId}>.\n${message.url}`);
+      this.#DTT.kanal("bugmail-discussion").send(`${interaction.user} has just claimed the free BugMail request of <@${this.userId}>.Be sure to post in <#733499719267123200> and ${this.#DTT.kanal("locked-reports")} for clarity!\n${message.url}`);
 
       interaction.update({
         content: `You have successfully claimed the free BugMail request of <@${this.userId}>!`,
