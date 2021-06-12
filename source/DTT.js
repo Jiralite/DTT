0303068444a0319b91bdfb89493074c74a247188
Jiralite <Jiralite@Live.co.uk>
2021-06-12 16:29:39 +0100

    fix(DTT): Close method brackets

--- source/DTT.js
+++ source/DTT.js
@@ -268,7 +268,7 @@ DTT.on("interaction", async interaction => {
     if (claimRequest && interaction.channelID === DTT.kanal("bugmail-queue").id) {
       if (claimRequest[2] === "PRECLAIM") return DTT.freeBugMails.get(+claimRequest[1]).preClaim(interaction, `${logText}"Claim" button `);
       if (claimRequest[2] === "CLAIM") return DTT.freeBugMails.get(+claimRequest[1]).claim(interaction, `${logText}"Yes! Claim!" button `);
-      if (claimRequest[2] === "BUGMAILED") return DTT.freeBugMails.get(+claimRequest[1]).alreadyBugMailed(interaction, `${logText}"Oops! It's BugMailed!" button `;
+      if (claimRequest[2] === "BUGMAILED") return DTT.freeBugMails.get(+claimRequest[1]).alreadyBugMailed(interaction, `${logText}"Oops! It's BugMailed!" button `);
     }
 
     const weekBugMail = /(\d+)-(PENDING|RESOLVED)/.exec(interaction.customID);
