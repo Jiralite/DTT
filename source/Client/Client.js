034e0a3d5a5a649374257949cbb2401639c88b77
Jiralite <Jiralite@Live.co.uk>
2021-06-12 17:20:14 +0100

    fix(Client): Define constant

--- source/Client/Client.js
+++ source/Client/Client.js
@@ -74,7 +74,7 @@ class DTT extends Client {
   async applyCommands() {
     await this.guild.commands.set([]);
 
-    for (command of this.commands) {
+    for (const command of this.commands) {
       const [applicationCommandData, permissions] = command.commandData;
       this.guild.create(applicationCommandData).then(createdCommand => createdCommand.setPermissions(permissions));
     }
