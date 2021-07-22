4d0ec71e7ba765ed062e7395efbc132ae9cd33c6
Jiralite <Jiralite@Live.co.uk>
2021-07-22 04:06:22 +0100

    fix(FreeBugMail): Follow up on interaction

--- source/Client/FreeBugMail.js
+++ source/Client/FreeBugMail.js
@@ -185,7 +185,7 @@ class FreeBugMail {
 
     this.#DTT.freeBugMailLog(`${interaction.user} is attempting to claim Free BugMail request #${this.No} and is being asked to double check before fully claiming.`);
 
-    interaction.reply({
+    interaction.followUp({
       content: "⚠️ Have you searched in Discord Testers (specifically <#733499719267123200>) to ensure that this isn't already BugMailed?",
       ephemeral: true,
       components: [
