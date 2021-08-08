9c7be3fb6b7579c5c8181fd3c301b5a911b61ef1
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-08-08 14:59:45 +0100

    feat: Add new Free BugMail interface

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,14 @@
+interface MariaFreeBugMail {
+  No: number;
+  Timestamp: number;
+  "Weekly Timestamp": number;
+  "Message ID": string;
+  "User ID": string;
+  "Claimed By ID": string;
+  Mentioned: boolean;
+  State: string;
+}
+
 interface MariaInvite {
   No: number;
   ID: string;
