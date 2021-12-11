b1948d07585f3913bbd48335bf88fcab6408cc1a
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-12-11 12:31:26 +0000

    refactor(Verification: Update code

--- source/Client/Verification.ts
+++ source/Client/Verification.ts
@@ -1,43 +1,35 @@
-import { ButtonInteraction, Constants, DiscordAPIError, GuildMember, Message, Snowflake, TextChannel } from "discord.js";
+import { ButtonInteraction, Constants, DiscordAPIError, Formatters, GuildMember, MessageActionRow, MessageButton, Snowflake, TextChannel } from "discord.js";
 import DTT from "./Client.js";
 
 export type VerificationType = "TESTER" | "ALT" | "DENY";
 
 export default class Verification {
   static sendVerification(guildMember: GuildMember): void {
+    const actionRow = new MessageActionRow();
+    const button = new MessageButton();
+    const button2 = new MessageButton();
+    const button3 = new MessageButton();
+    button.setCustomId(`TESTER-${guildMember.user.id}`);
+    button.setLabel("Tester");
+    button.setStyle(Constants.MessageButtonStyles.PRIMARY);
+    button2.setCustomId(`ALT-${guildMember.user.id}`);
+    button2.setLabel("Alt Account");
+    button2.setStyle(Constants.MessageButtonStyles.SECONDARY);
+    button3.setCustomId(`DENY-${guildMember.user.id}`);
+    button3.setLabel("Deny");
+    button3.setStyle(Constants.MessageButtonStyles.DANGER);
+    actionRow.addComponents(button, button2, button3);
 
     (DTT.channel("verification") as TextChannel).send({
       content: `Welcome to **${guildMember.guild.name}**, ${guildMember}! Please review the ${DTT.channel("read-me")} channel and a member of staff will approve your request to join. If you do not have access within several hours, feel free to send a direct message to <@873604718893617203>!`,
       components: [
-        {
-          type: "ACTION_ROW",
-          components: [
-            {
-              type: "BUTTON",
-              label: "Tester",
-              customId: `TESTER-${guildMember.user.id}`,
-              style: "PRIMARY"
-            },
-            {
-              type: "BUTTON",
-              label: "Alt Account",
-              customId: `ALT-${guildMember.user.id}`,
-              style: "SECONDARY"
-            },
-            {
-              type: "BUTTON",
-              label: "Deny",
-              customId: `DENY-${guildMember.user.id}`,
-              style: "DANGER"
-            }
-          ]
-        }
+        actionRow
       ]
     });
   }
 
-  static async authorise(interaction: ButtonInteraction, authentication: VerificationType, guildMemberId: Snowflake): Promise<void> {
-    if (!(interaction.member as GuildMember).roles.cache.hasAny(...DTT.modRoles.map((modRole => modRole.id)))) {
+  static async authorise(interaction: ButtonInteraction<"cached">, authentication: VerificationType, guildMemberId: Snowflake): Promise<void> {
+    if (!interaction.member.roles.cache.hasAny(...DTT.modRoles.map((modRole => modRole.id)))) {
       DTT.log(`${interaction.user} interacted with a verification button but failed authorisation checks.`);
 
       return interaction.reply({
@@ -60,13 +52,13 @@ export default class Verification {
     } catch (error) {
       if (error instanceof DiscordAPIError) {
         if (error.code === Constants.APIErrors.UNKNOWN_MEMBER) {
-          const guildMemberManualMention = `<@${guildMemberId}>`;
+          const guildMemberManualMention = Formatters.userMention(guildMemberId);
           DTT.log(`Error during verification: ${guildMemberManualMention} no longer in server.`);
 
           interaction.update({
-            content: `${guildMemberManualMention} no longer appears to be in the server.`,
-            components: []
-          }).then(() => setTimeout(() => (interaction.message as Message).delete().catch(() => null), 10000));
+            components: [],
+            content: `${guildMemberManualMention} appears to no longer be in the server.`
+          }).then(() => setTimeout(() => interaction.message.delete().catch(() => null), 10000));
 
           return;
         }
@@ -81,7 +73,7 @@ export default class Verification {
     }
   }
 
-  static authoriseTester(interaction: ButtonInteraction, guildMember: GuildMember): void {
+  static authoriseTester(interaction: ButtonInteraction<"cached">, guildMember: GuildMember): void {
     const tester = DTT.role("Tester");
 
     guildMember.roles.add(tester).then(() => {
@@ -92,7 +84,7 @@ export default class Verification {
         ephemeral: true
       });
 
-      (interaction.message as Message).delete().catch(() => null);
+      interaction.message.delete().catch(() => null);
       (DTT.channel("general") as TextChannel).send(`Welcome to **${DTT.guild.name}**, ${guildMember}! Be sure to check out the channels in this server!`);
     }).catch(error => {
       DTT.log(`Error adding role to ${guildMember}.`, error);
@@ -104,7 +96,7 @@ export default class Verification {
     });
   }
 
-  static authoriseAlt(interaction: ButtonInteraction, guildMember: GuildMember): void {
+  static authoriseAlt(interaction: ButtonInteraction<"cached">, guildMember: GuildMember): void {
     const altAccount = DTT.role("Alt Account");
 
     guildMember.roles.add(altAccount).then(() => {
@@ -115,7 +107,7 @@ export default class Verification {
         ephemeral: true
       });
 
-      (interaction.message as Message).delete().catch(() => null);
+      interaction.message.delete().catch(() => null);
     }).catch(error => {
       DTT.log(`Error adding role to ${guildMember}.`, error);
 
@@ -126,12 +118,12 @@ export default class Verification {
     });
   }
 
-  static authoriseKick(interaction: ButtonInteraction, guildMember: GuildMember): void {
+  static authoriseKick(interaction: ButtonInteraction<"cached">, guildMember: GuildMember): void {
 
     guildMember.kick().then(() => {
       DTT.log(`${interaction.user} has removed ${guildMember} from this guild - failed verification.`);
       interaction.reply(`${interaction.user} has kicked ${guildMember}.`);
-      (interaction.message as Message).delete().catch(() => null);
+      interaction.message.delete().catch(() => null);
       setTimeout(() => interaction.deleteReply().catch(() => null), 60000);
     }).catch(error => {
       DTT.log(`Error removing ${guildMember} from this guild.`, error);
