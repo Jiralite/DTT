4385bac600d44cabf138b447398471a458b2dd30
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-12-11 01:49:00 +0000

    refactor(DTT): Use `URL`

--- source/DTT.ts
+++ source/DTT.ts
@@ -4,7 +4,7 @@ import DTT from "./Client/Client.js";
 const eventsPath = new URL("./Events/", import.meta.url);
 
 for (const file of readdirSync(eventsPath).filter(file => file !== "index.js")) {
-  const { name, once, fire } = (await import(eventsPath + file)).event;
+  const { name, once, fire } = (await import(new URL(file, eventsPath).toString())).event;
   DTT[once ? "once" : "on"](name, fire);
 }
 
