af135941a1f209eea53258591298150c38d18e79
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-02-07 18:33:49 +0000

    fix: Encode branch name in URL

--- source/Events/ready.ts
+++ source/Events/ready.ts
@@ -119,7 +119,7 @@ export const event: Event<typeof name> = {
     embed.setDescription(
       `Running [\`${commitHash.slice(0, 7)}\`](${
         json.html_url
-      }) on [\`${branch}\`](https://github.com/${repository}/tree/${branch}) at ${Formatters.time(
+      }) on [\`${branch}\`](https://github.com/${repository}/tree/${encodeURIComponent(branch)}) at ${Formatters.time(
         Math.floor(Date.now() / 1000),
         Formatters.TimestampStyles.ShortDateTime
       )}.\n${json.commit.message} - [${json.author.login}](${json.author.html_url})`
